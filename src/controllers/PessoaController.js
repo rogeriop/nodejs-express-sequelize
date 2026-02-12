983c9c849d64c918203fa2336cf9c1af8c8803a4
rogeriop <pinheiroxrogerio@gmail.com>
2026-02-12 09:59:54 -0300

    05.Atualizando a API

--- src/controllers/PessoaController.js
+++ src/controllers/PessoaController.js
@@ -9,6 +9,15 @@ class PessoaController extends Controller {
         super(pessoaServices);
     }
 
+    async pegaMatriculas(req, res) {
+        const { estudanteId } = req.params;
+        try {
+          const listaMatriculas = await pessoaServices.pegaMatriculasPorEstudante(Number(estudanteId));
+          return res.status(200).json(listaMatriculas); 
+        } catch (erro) {
+           // erro 
+        }
+    }
 }
 
 module.exports = PessoaController;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const pessoas = require('./pessoasRouter.js');
+const categorias = require('./categoriasRouter.js');
+const cursos = require('./cursosRouter.js');
 
 module.exports = app => {
     app.use(
         express.json(),
         pessoas,
+        categorias,
+        cursos
+  
     );
 };
 
